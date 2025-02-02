267321a779c68de40363780c64025c837fe670ef
PenguinHerder <skutelaandrzej@yahoo.com>
2025-02-02 01:40:23 +0000

    landing text

--- resources/js/Pages/Ethics.vue
+++ resources/js/Pages/Ethics.vue
@@ -10,16 +10,17 @@ defineProps({ data: Object })
 <template>
   <Layout>
     <Head title="Cognitive Psychology Research Project" />
-    <h1 class="text-center text-2xl">Impact of Different Preparation Methods on Visual Short-term Memory</h1>
-    <p class="text-lg my-4 text-justify">I want you to play a simple memory game. You will be shown a single, positive image and all you need to do is tap it, or click it. Once you do so,
-    the same image will appear on the list accompanied with another image in similar theme. I want you to select the new image that you have not seen before. This will continue, until either,
-    you successfuly select the newly-added image, 40 times, or make a mistake. The score will always be shown to you. Remember to always select the image that has not yet been displayed to you.</p>
-    <p class="text-lg my-4 text-justify">You will have three trials, each time with different set of images: male faces, female faces, and nature landscapes. The order in which
+    <h1 class="text-center text-2xl pb-2">Impact of Different Preparation Methods on Visual Short-term Memory</h1>
+    <p class="text-xl my-6 text-justify">I want you to play a simple memory game. You will be shown a single, positive image and all you need to do is tap it, or click it. Once you do so,
+    the same image will appear on the list accompanied with another image in similar theme. I want you to select the <span class="italic">*new*</span> image that you have not seen before. This will continue, until either,
+    you successfuly select the newly-added image 40 times, or make a mistake. The score will always be shown to you and you will have 30 seconds each time to select an image.
+    Remember to always select the image that has <span class="italic">*not*</span> yet been displayed to you.</p>
+    <p class="text-xl my-6 text-justify">You will have three trials, each time with different set of images: male faces, female faces, and nature landscapes. The order in which
     you see these images is random. In the next page, you will have the opportunity to familiarise yourself with the system.
     You will be presented with the game, using a set of only three flower images.
     This trial is NOT part of the task, it is only there to show you how the actual task works. Once you familiarise yourself with the game, you will be redirected to the task's landing page.</p>
-    <p class="text-lg my-4 text-justify">From this point on, please do not refresh the page or go back, as this will invalidate your results.</p>
-    <p class="text-lg my-4 text-justify">If you wish to continue please tap, or click the button below. If you wish to withdraw, simply close this page.</p>
+    <p class="text-xl my-6 text-justify">From this point on, please do not refresh the page or go back, as this will invalidate your results.</p>
+    <p class="text-xl my-6 text-justify">If you wish to continue please tap, or click the button below. If you wish to withdraw, simply close this page.</p>
     <div class="text-center my-8">
       <fwb-button gradient="green" :href="data.continue_link" class="text-lg">I agree to continue</fwb-button>
     </div>
--- resources/js/Pages/Landing.vue
+++ resources/js/Pages/Landing.vue
@@ -10,15 +10,15 @@ defineProps({ data: Object })
 <template>
   <Layout>
     <Head title="Cognitive Psychology Research Project" />
-    <h1 class="text-center text-4xl">Welcome</h1>
-    <p class="text-lg my-4 text-justify">Thank you for visiting this website. Please read the following introductions carefully before proceding:</p>
-    <p class="text-lg mb-4 text-justify">The following pages contain a short task designed to measure the capacity of your visual short-term memory in relation to a variable preparation.
+    <h1 class="text-center text-4xl pb-2">Welcome</h1>
+    <p class="text-xl my-6 text-justify">Thank you for visiting this website. Please read the following introductions carefully before proceding:</p>
+    <p class="text-xl mb-6 text-justify">The following pages contain a short task designed to measure the capacity of your visual short-term memory in relation to a variable preparation.
     The exact instructions will be given in the next page, but before you continue I ask you to have 15 minutes of uninterupted time in a quiet environment.
-    You can perform the task on your computer or tablet, altough phone is prefered. I also request you to have headphones ready for the audible part of the task.</p>
-    <p class="text-lg mb-4 text-justify">The entire task will take no more than 15 minutes, and you retain the right to withdraw at any time.
+    You can perform the task on your computer or tablet, altough phone is preferred. I also request you to have headphones ready for the audible part of the task.</p>
+    <p class="text-xl mb-6 text-justify">The entire task will take no more than 15 minutes to complete, and you retain the right to withdraw at any time.
     The research is conducted in line with current ethical guidelines of the British Psychological Society and you will come to no harm.</p>
-    <p class="text-lg mb-4 text-justify">I hope you will be a willing participant and enjoy the task. Once the research is finished I am happy to share the findings with you if you wish so.</p>
-    <div class="text-center my-8">
+    <p class="text-xl mb-6 text-justify">I hope you will be a willing participant and enjoy the task. Once the research is finished I am happy to share the findings with you if you wish so.</p>
+    <div class="text-center my-12">
       <fwb-button gradient="green" :href="data.continue_link" class="text-lg">I want to participate</fwb-button>
     </div>
   </Layout>
