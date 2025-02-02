15ec5b0dbf08f68d7ccf7cca5ea7ecd71c4ad6c6
PenguinHerder <skutelaandrzej@yahoo.com>
2025-02-02 02:17:41 +0000

    add leg field

--- database/migrations/2025_02_01_233532_data_leads.php
+++ database/migrations/2025_02_01_233532_data_leads.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('data_leads', function (Blueprint $table) {
             $table->id();
             $table->string('uuid')->unique();
+            $table->tinyInteger('leg');
             $table->string('email')->nullable();
             $table->boolean('is_new_browser');
             $table->string('ip');
